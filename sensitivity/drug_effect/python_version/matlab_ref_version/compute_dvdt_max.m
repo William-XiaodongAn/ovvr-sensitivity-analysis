eaa8705f42c469b367d8fc451beb54d1a7283b58
William-XiaodongAn <85208832+William-XiaodongAn@users.noreply.github.com>
2026-06-04 15:41:43 -0400

    1

--- sensitivity/drug_effect/python_version/matlab_ref_version/compute_dvdt_max.m
+++
@@ -1,8 +0,0 @@
-function dvdt = compute_dvdt_max(V, T)
-%dvdt = compute_dvdt_max(V, T)
-%   Compute the maximum upstroke velocity
-
-dvdt = max((V(2:end)-V(1:end-1))./(T(2:end)-T(1:end-1)));
-
-end
-
